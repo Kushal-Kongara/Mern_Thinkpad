f1712c15519d76857b751eab82d05df0b97f3a7a
kushalkongara <kkushal2509@gmail.com>
2025-08-28 12:17:51 -0700

    env file, model folder, config folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+connectDB();
 
 app.use(express.json());
 app.use("/api/notes", notesRoutes);
