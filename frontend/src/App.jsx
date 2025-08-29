04bf13e4cba44c02a813100e8c2c152ba3f7f7d1
kushalkongara <kkushal2509@gmail.com>
2025-08-28 23:24:01 -0700

    Add/track frontend under root repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+
+import HomePage from "./pages/HomePage.jsx"
+import CreatePage from "./pages/CreatePage.jsx"
+import NoteDetilPage from "./pages/NoteDetailPage.jsx"
+import toast from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div data-theme="garden">
+      <button className='btn btn-outline'>Click me</button>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/create' element={<CreatePage/>}/>
+        <Route path='/note/:id' element={<NoteDetilPage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
