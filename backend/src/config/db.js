29940fcc00ff3d36694e84d975b8588a1d76bd08
kushalkongara <kkushal2509@gmail.com>
2025-08-28 20:48:37 -0700

    Project root: backend + frontend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Successful")
+        console.log("MongoDB connected Successfully")
     } catch (error) {
         console.error("Error connecting to MongoDB", error)
         process.exit(1) //exit with failure
